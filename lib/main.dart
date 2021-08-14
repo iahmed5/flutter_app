e37b611363ac258a41ffa828b21cd2afb7c89a8b
iahmed5 <izaz.ahmed1@hotmail.com>
2021-08-14 22:00:33 +0100

    business card

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50,
-                backgroundImage: AssetImage('images/iahmed.jpg'),
+                backgroundImage: AssetImage('images/profile-picture.jpg'),
               ),
               Text(
-                'Izaz Ahmed',
+                'I Ahmed',
                 style: TextStyle(
                     fontFamily: 'Pacifico',
                     fontSize: 40,
@@ -34,41 +35,41 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     letterSpacing: 2.5),
               ),
+              SizedBox(
+                  height: 20,
+                  width: 150,
+                  child: Divider(color: Colors.teal[100])),
               Card(
-                color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: Padding(
-                  padding: EdgeInsets.all(25),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.phone, color: Colors.teal),
-                      SizedBox(width: 10),
-                      Text('+44 74 1234 5678',
-                          style: TextStyle(
-                            fontFamily: 'Source Sans Pro',
-                            fontSize: 20,
-                            color: Colors.teal[900],
-                          )),
-                    ],
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+44 74 1234 5678',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                      color: Colors.teal[900],
+                    ),
                   ),
                 ),
               ),
               Card(
-                color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: Padding(
-                  padding: EdgeInsets.all(25),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.email, color: Colors.teal),
-                      SizedBox(width: 10),
-                      Text('iahmed@yahoo.com',
-                          style: TextStyle(
-                            fontFamily: 'Source Sans Pro',
-                            fontSize: 20,
-                            color: Colors.teal[900],
-                          )),
-                    ],
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'iahmed5@yahoo.com',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                      color: Colors.teal[900],
+                    ),
                   ),
                 ),
               )
